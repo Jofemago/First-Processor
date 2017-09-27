e413039ea16d7cfc41d6bbc9a7fc755919a02993
Jofemago <jofemago@utp.edu.co>
2017-09-27 16:48:51 -0500

    Add files via upload
    Sumador de 32 bits y Registo para Program counter y Next Program Counter

---
+++ register.vhd
@@ -0,0 +1,44 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity regis is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC := '0' ;
+           datain : in  STD_LOGIC_VECTOR (31 downto 0) := x"00000000" ;
+           dataout : out  STD_LOGIC_VECTOR (31 downto 0) := x"00000000" );
+end regis;
+
+architecture Behavioral of regis is
+
+signal res: std_logic_vector(31 downto 0):= x"00000000";
+
+begin
+res <= datain;
+
+
+	process (clk, rst)
+		begin  
+			if (clk'event and clk = '1') then
+				dataout <= res;
+			end if;
+			if (rst = '1') then
+		      dataout <= "00000000000000000000000000000000";
+		   end if;
+		
+	end process;
+	
+
+
+--process (rst)
+--begin  
+--	if (rst'event and rst = '1') then
+--		res <= x"00000000";
+--		dataout <= res;
+--	end if;
+--end process;
+
+end Behavioral;
+
