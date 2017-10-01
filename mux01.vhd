7ef0a88bda63e9380935b26fea2e48605b3f360e
bravo95 <andresfelipe.bravo@utp.edu.co>
2017-10-01 14:20:37 -0500

    multiplexor
    multiplexor 1 o 0 hay un inconveniente cuando empieza la simulacion, no pone el valor que se necesita que ponga. Corregir

---
+++ mux01.vhd
@@ -0,0 +1,27 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+
+
+entity mux01 is
+    Port ( crs2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           i : in  STD_LOGIC;
+           sevout : in  STD_LOGIC_VECTOR (31 downto 0);
+           muxout : out  STD_LOGIC_VECTOR (31 downto 0));
+end mux01;
+
+architecture Behavioral of mux01 is
+
+begin
+	process (i)
+		begin
+		if (i = '1') then
+			muxout <= sevout;
+		else
+			muxout <= crs2;
+		end if;
+	end process;
+end Behavioral;
+
